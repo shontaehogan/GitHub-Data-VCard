6b080047608b3b1e193d94f3841b5075aa2f743d
Shontae Hogan <shontaehogan@gmail.com>
2017-07-18 13:52:12 -0400

    changes made to the css

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,11 @@ let body = document.querySelector('.body');
 
 let request = new XMLHttpRequest();
 
-// when data from the request is loaded, run the function "githubData"
-request.addEventListener("load", githubData);
 // define the request to read the data from this gitHub link
 request.open('GET', 'https://api.github.com/users/shontaehogan');
+
+// when data from the request is loaded, run the function "githubData"
+request.addEventListener("load", githubData);
 // send request to the specified github url
 request.send();
 
@@ -21,12 +22,12 @@ function githubData() {
   // console.log(info);
   // console.log(info.name);
 
-
+console.log(info);
 
   let form = `
-    <div class = boxTop>
+    <header class = boxTop>
     <h1>${info.name}</h1>
-    </div>
+    </header>
 
     <section class = boxBody>
       <div class = basics>
@@ -34,12 +35,9 @@ function githubData() {
           <ul>
             <li><span class="text">Name: </span> ${info.name}</li>
             <li><span class="link">GitHub URL:</span><a href="${info.html_url}"> shogan</a></li>
+            <li><span class="text">Email: </span><a href= "${info.email}">shogan@gmail.com</a></li>
             <li><span class="text">Company: </span> ${info.company}</li>
-            <li><span class="link">Website: </span><a href= "${info.url}"> www.myawesomewebsite.com</a></li>
-
-
-
-
+            <li><span class="link">Website: </span><a href= "${info.url}"> www.mysite.com</a></li>
           </ul>
       </div>
 
@@ -49,6 +47,7 @@ function githubData() {
         <h2>The Story</h2>
           <p>${info.bio}</p>
       </div>
+
       <div class = picture>
         <img src="${info.avatar_url}">
       </div>
